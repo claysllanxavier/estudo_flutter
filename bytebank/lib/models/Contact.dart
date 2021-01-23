525fd56a1f5ee330917276f3353d597ea3935968
Claysllan Xavier <claysllan@gmail.com>
2021-01-23 12:32:25 -0300

    ✨ Configura e testa Sqlite na aplicação

--- bytebank/lib/models/Contact.dart
+++ bytebank/lib/models/Contact.dart
@@ -1,11 +1,12 @@
 class Contact {
+  final int id;
   final String name;
   final int accountNumber;
 
-  Contact(this.name, this.accountNumber);
+  Contact(this.id, this.name, this.accountNumber);
 
   @override
   String toString() {
-    return 'Contato(nome: $name, telefone: $accountNumber)';
+    return 'Contato(id: $id, nome: $name, telefone: $accountNumber)';
   }
 }
