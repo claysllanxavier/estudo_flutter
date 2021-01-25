5a4d5270d37b1aca823d94399c23df5752c3c49e
Claysllan Xavier <claysllan@gmail.com>
2021-01-25 11:05:04 -0300

    ✨  Implementa webclient para conexões http
    - Configura métodos de busca e salvamento de transações pela api
    - Integra a lista de transações com os dados vindos da api

--- bytebank/lib/screens/contact_list.dart
+++ bytebank/lib/screens/contact_list.dart
@@ -1,3 +1,4 @@
+import 'package:bytebank/components/Progress.dart';
 import 'package:bytebank/dao/contact_dao.dart';
 import 'package:bytebank/models/Contact.dart';
 import 'package:bytebank/screens/contact_form.dart';
@@ -25,16 +26,7 @@ class _ContactListState extends State<ContactList> {
             case ConnectionState.none:
               break;
             case ConnectionState.waiting:
-              return Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    CircularProgressIndicator(),
-                    Text('Loading'),
-                  ],
-                ),
-              );
+              return Progress();
               break;
             case ConnectionState.active:
               break;
