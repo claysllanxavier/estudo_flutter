db8e80eb114bb84d731f93c30ea51bfb86833e24
Claysllan Xavier <claysllan@gmail.com>
2021-01-25 11:50:21 -0300

    ♻️  Simplifica códigod e conversão
    - Utiliza a função map() para converter uma lista.

--- bytebank/lib/http/webclients/transaction_webclient.dart
+++ bytebank/lib/http/webclients/transaction_webclient.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 
 import 'package:bytebank/http/webclient.dart';
-import 'package:bytebank/models/Contact.dart';
 import 'package:bytebank/models/Transiction.dart';
 import 'package:http/http.dart';
 
@@ -10,9 +9,11 @@ class TransactionWebClient {
     final Response response =
         await client.get(baseUrl).timeout(Duration(seconds: 5));
 
-    List<Transaction> transactions = _toTransactions(response);
+    final List<dynamic> decodedJson = jsonDecode(response.body);
 
-    return transactions;
+    return decodedJson
+        .map((dynamic json) => Transaction.fromJson(json))
+        .toList();
   }
 
   Future<Transaction> save(Transaction transaction) async {
@@ -27,25 +28,6 @@ class TransactionWebClient {
       body: transactionJson,
     );
 
-    Transaction newTransaction = _toTransaction(response);
-
-    return newTransaction;
-  }
-
-  Transaction _toTransaction(Response response) {
-    Map<String, dynamic> json = jsonDecode(response.body);
-
-    return Transaction.fromJson(json);
-  }
-
-  List<Transaction> _toTransactions(Response response) {
-    final List<Transaction> transactions = List();
-
-    final List<dynamic> decodedJson = jsonDecode(response.body);
-
-    for (Map<String, dynamic> transactionJson in decodedJson) {
-      transactions.add(Transaction.fromJson(transactionJson));
-    }
-    return transactions;
+    return Transaction.fromJson(jsonDecode(response.body));
   }
 }
