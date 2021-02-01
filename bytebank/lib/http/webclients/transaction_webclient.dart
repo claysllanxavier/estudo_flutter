88411c328f2f6d702de384b8d7026ca5192f2be7
Claysllan Xavier <claysllan@gmail.com>
2021-02-01 11:18:31 -0300

    ✨  Evitando transferências duplicadas
    - Cria um id únicos para cada transferência

--- bytebank/lib/http/webclients/transaction_webclient.dart
+++ bytebank/lib/http/webclients/transaction_webclient.dart
@@ -31,12 +31,21 @@ class TransactionWebClient {
       return Transaction.fromJson(jsonDecode(response.body));
     }
 
-    throw HttpException(_statusCodeResponse[response.statusCode]);
+    throw HttpException(_getMessage(response.statusCode));
+  }
+
+  String _getMessage(int statusCode) {
+    if (_statusCodeResponse.containsKey(statusCode)) {
+      return _statusCodeResponse[statusCode];
+    }
+
+    return 'Unknow error.';
   }
 
   static final Map<int, String> _statusCodeResponse = {
     400: 'there was an error submitting transaction.',
-    401: 'authentication failed.'
+    401: 'authentication failed.',
+    409: 'transaction always exists.'
   };
 }
 
